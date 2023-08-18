682a108caaae2685d3f2b46145a9920353412095
henrypilotinho <henriquepilotl@hotmail.com>
2023-08-18 09:36:21 -0300

    att

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.get("/", function(req, res){
     res.send("Ta certinho")
 })
 
+app.get("/contato", function(req, res){
+    res.send("os contatinho")
+})
+
+app.get("/produtos/:item/:qtd", function(req, res){
+    res.send("item: " + req.params.item + "<br> Quantidade: " + req.params.qtd)
+})
 
 app.listen(8081, function(){
         console.log("servidor rodando na porta 8081")
