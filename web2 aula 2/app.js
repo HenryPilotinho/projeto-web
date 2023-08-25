d18158b745419ab40593ce259de7366bba00bfb6
Henry <131377083+HenryPilotinho@users.noreply.github.com>
2023-08-25 09:36:09 -0300

    att

--- web2 aula 2/app.js
+++ web2 aula 2/app.js
@@ -1,15 +1,40 @@
-const express = require ("express")
+const express = require("express")
 const app = express()
 
-
-const Sequelize = require ("sequelize")
-const sequlize = new Sequelize("aulinha","root","", {
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize("aulinha","root","", {
     host:"localhost",
     dialect: "mysql"
 })
 
-sequlize.authenticate().then(function(){
+sequelize.authenticate().then(function(){
     console.log("ta certinho")
 }).catch(function(erro){
     console.log("deu merda "+erro)
 })
+
+const agendamentos = sequelize.define("agendamentos",{
+   nome:{
+        type: Sequelize.STRING 
+   },
+   endereço:{
+        type: Sequelize.STRING 
+   },
+   bairro:{
+        type: Sequelize.STRING 
+   },
+   cep:{
+        type: Sequelize.INTEGER 
+   },
+   cidade:{
+        type: Sequelize.STRING 
+    },
+   estado:{
+        type: Sequelize.STRING 
+    },
+   observação:{
+        type: Sequelize.TEXT 
+   },
+})
+
+//agendamentos.sync({force: true})
